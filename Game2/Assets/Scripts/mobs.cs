16535140723c84686670860ab44e2544b3d9d605
Mathieu Fenyes <mathieufenyes@MacBook-Pro.local>
2020-07-20 16:00:12 +0200

    mobs moovements + ligths

---
+++ Game2/Assets/Scripts/mobs.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class mobs : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private GameObject Player;
+    private float Range;
+
+    [SerializeField]
+    private float speedMultiplier;
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag ("Player");
+    }
+    
+    void Update()
+    { 
+        Range = Vector2.Distance(transform.position, Player.transform.position);
+        if (Range <= 10f)
+         {
+             transform.position = Vector2.MoveTowards(transform.position, Player.transform.position, speedMultiplier * Time.deltaTime);
+         }
+    }
+}
