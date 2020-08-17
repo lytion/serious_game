749ea3f7de13d541f6ddee43b925d375af3828d2
Simon <bauchet.simon@gmail.com>
2020-08-17 17:22:01 +0200

    mob random move

--- Game2/Assets/Scripts/mobs.cs
+++ Game2/Assets/Scripts/mobs.cs
@@ -1,5 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
+using Random = System.Random;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class mobs : MonoBehaviour
@@ -10,21 +12,49 @@ public class mobs : MonoBehaviour
     public int health;
     private QuestionUtilities _questionUtilities;
 
+    public Transform[] moveSpot;
+    private int randomSpot;
+    private float waitTime;
+    public float startWaitTime;
+
     [SerializeField]
     private float speedMultiplier;
     void Start()
     {
         Player = GameObject.FindGameObjectWithTag ("Player");
         _questionUtilities = GameObject.FindWithTag("GameManager").GetComponent<QuestionUtilities>();
+        waitTime = startWaitTime;
+        if (moveSpot == null)
+            return;
+        randomSpot = new System.Random().Next(0, moveSpot.Length);
     }
     
     void Update()
     {
         Range = Vector2.Distance(transform.position, Player.transform.position);
-        if (Range <= 4f)
-         {
-             transform.position = Vector2.MoveTowards(transform.position, Player.transform.position, speedMultiplier * Time.deltaTime);
-         }
+        if (Range <= 4f) 
+        { 
+            transform.position = Vector2.MoveTowards(transform.position, Player.transform.position, speedMultiplier * Time.deltaTime);
+        }
+        else
+        {
+            if (moveSpot == null || moveSpot.Length == 0)
+                return;
+            transform.position =
+                Vector2.MoveTowards(transform.position, moveSpot[randomSpot].position, speedMultiplier * Time.deltaTime);
+            if (Vector2.Distance(transform.position, moveSpot[randomSpot].position) < 0.2f)
+            {
+                if (waitTime <= 0)
+                {
+                    randomSpot = new System.Random().Next(0, moveSpot.Length);
+                    waitTime = startWaitTime;
+                }
+                else
+                {
+                    waitTime -= Time.deltaTime;
+                }
+            }
+        }
     }
     
     void OnTriggerEnter2D(Collider2D other)
